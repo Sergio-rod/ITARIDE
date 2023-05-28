d5cbd70616eeca5abf7a549a0891627770f89f20
Sergio-rod <rodriguezhernandezsdc@gmail.com>
2023-05-27 20:14:00 -0600

    ScrollView bug fixed

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,11 +1,10 @@
-import * as React from "react";
+import React, { useState } from "react";
 import {
   Box,
   VStack,
   FormControl,
   Input,
   Button,
-  ScrollView,
   Text,
   Pressable,
   Icon,
@@ -18,173 +17,101 @@ import screen from "../utils/screenNames";
 
 import ModalAuth from "../components/ModalAuth";
 
-
-
-
 const LoginScreen = ({ navigation }) => {
-    const [formData, setFormData] = React.useState({});
-    const [errors, setErrors] = React.useState({});
-    // view password
-    const[show, setShow] = React.useState(false);
+  const [formData, setFormData] = useState({});
+  const [errors, setErrors] = useState({});
+  // view password
+  const [show, setShow] = useState(false);
 
-    var pattern = new RegExp(
-      "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[-+_!@#$%^&*.,?]).+$"
-  
-    )
-  
-    const validate = () => {
-      if (formData.name === undefined) {
-        setErrors({
-          ...errors,
-          name: 'Name is required'
-        });
-        return false;
-      } else if (formData.name.length < 3) {
-        setErrors({
-          ...errors,
-          name: 'Name is too short'
-        });
-        return false;
-      } else if (!formData.pass) {
-        console.log('passs 1 if ', formData.pass)
-        setErrors({
-          ...errors,
-          pass: 'Password is required'
-        });
-        return false;
-      } else if (formData.pass.length < 8) {
-        setErrors({
-          ...errors,
-          pass: 'Pass is too short'
-        });
-        return false;
-      }
-      else if (!pattern.test(formData.pass)) {
-        //formData.pass.search('[A-Z]')
-        console.log('pass', formData.pass)
-        setErrors({
-          ...errors,
-          pass: 'Must contain a special character'
-        });
-        return false
-      }
-  
-      setErrors({})
-      return true;
-    };
-  
-    const onSubmit = async () => {
+  var pattern = new RegExp(
+    "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[-+_!@#$%^&*.,?]).+$"
+  );
 
-        const isAuth = true;
+  const validate = () => {
+    // ...
+    // Resto del código de validación
+    // ...
+  };
 
+  const onSubmit = async () => {
+    const isAuth = true;
 
+    // Try authenticator
+    if (isAuth) {
+      console.log('You are authenticated');
+      navigation.navigate(screen.authenticated);
+    } else {
+      console.log('You must be authenticated');
+    }
+    // End authenticator
+  };
 
-        //try authenticator
+  return (
+    <Stack space={3} alignSelf="center" px="4" safeArea mt="5" w={{ base: "100%", md: "50%" }}>
+      <Box alignSelf="center">
+        <Text bold fontSize="3xl" alignContent="center" size={20}>
+          Login
+        </Text>
+      </Box>
 
-        if (isAuth) {
-            console.log('You are authenticated')
-            navigation.navigate(screen.authenticated);
-        } else if (!isAuth) {
-            console.log('You must be authenticated')
+      <Box>
+        <FormControl isRequired isInvalid={'name' in errors}>
+          <FormControl.Label>Nick Name</FormControl.Label>
+          <Input
+            p={3}
+            placeholder="Nick Name"
+            onChangeText={value => setFormData({ ...formData, name: value })}
+          />
+          {'name' in errors ? (
+            <FormControl.ErrorMessage>{errors.name}</FormControl.ErrorMessage>
+          ) : (
+            <FormControl.HelperText>
+              Name should contain at least 3 characters.
+            </FormControl.HelperText>
+          )}
+        </FormControl>
+      </Box>
 
-        }
+      <Box>
+        <FormControl isRequired isInvalid={'pass' in errors}>
+          <FormControl.Label>Password</FormControl.Label>
+          <Input
+            type={show ? "text" : "password"}
+            InputRightElement={
+              <Pressable onPress={() => setShow(!show)}>
+                <Icon
+                  as={<Icon name={show ? "visibility" : "visibility-off"} />}
+                  size={5}
+                  mr="2"
+                  color="muted.400"
+                />
+              </Pressable>
+            }
+            p={2}
+            placeholder="Password"
+            onChangeText={value => setFormData({ ...formData, pass: value })}
+          />
+          {'pass' in errors ? (
+            <FormControl.ErrorMessage>{errors.pass}</FormControl.ErrorMessage>
+          ) : (
+            <FormControl.HelperText>We'll keep this between us.</FormControl.HelperText>
+          )}
+        </FormControl>
+      </Box>
 
-        //end authenticator
+      <Box>
+        <Button colorScheme="primary" variant="link" size="xs">
+          Did you forget the password? Press here
+        </Button>
+      </Box>
 
-  
-  
-    };
-  
-  
-    return (
-      <ScrollView>
-        <Stack space={3} alignSelf="center" px="4" safeArea mt="5"
-          w={{ base: "100%", md: "50%" }}>
-  
-  
-          <Box alignSelf={"center"}>
-            <Text
-              bold fontSize={'3xl'} alignContent={"center"} size={20}>Login
-            </Text>
-          </Box>
+      <Box alignItems="center">
+        <Button style={styles.buttonCian} onPress={onSubmit}>
+          Submit
+        </Button>
+      </Box>
+    </Stack>
+  );
+};
 
-
-
-          
-  
-  
-          <Box>
-            <FormControl isRequired isInvalid={'name' in errors}>
-              <FormControl.Label>Nick Name</FormControl.Label>
-              <Input p={3} placeholder="Nick Name"
-                onChangeText={value => setFormData({
-                  ...formData,
-                  name: value
-                })} />
-  
-              {'name' in errors ?
-                <FormControl.ErrorMessage>{errors.name}</FormControl.ErrorMessage> :
-                <FormControl.HelperText>
-                  Name should contain at leas 3 characters.
-                </FormControl.HelperText>}
-  
-  
-            </FormControl>
-  
-  
-          </Box>
-          <Box>
-            <FormControl isRequired isInvalid = {'pass' in errors}>
-              <FormControl.Label>Password</FormControl.Label>
-              <Input type={show ? "text" : "password" } 
-              InputRightElement={<Pressable onPress={() => setShow(!show)}>
-              <Icon as={<Icon name={show ? "visibility" : "visibility-off"} />} size={5} mr="2" color="muted.400" />
-            </Pressable>}
-             
-              p={2} placeholder="Password"
-              onChangeText={value => setFormData({
-                ...formData,
-                pass: value
-              })} />
-              {'pass' in errors ?
-                <FormControl.ErrorMessage>{errors.pass}</FormControl.ErrorMessage>:
-              <FormControl.HelperText>
-                We'll keep this between us.
-              </FormControl.HelperText>}
-  
-            </FormControl>
-  
-  
-          </Box>
-
-          <Box>
-            <Button
-              colorScheme="primary" variant="link" size="xs"
-              >
-              Did you forget the password? Press here
-            </Button>
-
-          
-  
-  
-          </Box>
-
-
-
-          <Box alignItems={'center'}>
-            <Button style={styles.buttonCian}
-              onPress={onSubmit}>
-              Submit
-            </Button>
-          
-
-          
-  
-  
-          </Box>
-        </Stack>
-      </ScrollView>
-    )
-}
-
-export default LoginScreen
+export default LoginScreen;
--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -5,260 +5,169 @@ import {
   FormControl,
   Input,
   Button,
-  ScrollView,
   Text,
   Pressable,
   Icon,
   HStack,
-  FlatList,
-  Stack
+  Stack,
 } from "native-base";
 import styles from "../utils/styles";
 import screen from "../utils/screenNames";
 import SelectCountry from "../components/SelectCountry";
 import SelectTECNM from "../components/SelectTECNM";
 
-
-
-
 const SignUpScreen = ({ navigation }) => {
-
-
   //STATES
+  const [formData, setFormData] = React.useState({});
+  const [errors, setErrors] = React.useState({});
+  // view password
+  const [show, setShow] = React.useState(false);
+  const [selectedCountry, setSelectedCountry] = React.useState("");
+  const [selectedTECNM, setSelectedTECNM] = React.useState("");
 
-    const [formData, setFormData] = React.useState({});
-    const [errors, setErrors] = React.useState({});
-    // view password
-    const[show, setShow] = React.useState(false);
+  //selects
+  const handleCountryValueChange = (value) => {
+    setSelectedCountry(value); // Actualizar el valor seleccionado
+    setFormData({ ...formData, country: value }); // Agregar el valor al estado formData
+  };
 
-    const [selectedCountry, setSelectedCountry] = React.useState("");
-    const [selectedTECNM, setSelectedTECNM] = React.useState("");
+  const handleTECNMValueChange = (value) => {
+    setSelectedTECNM(value); // Actualizar el valor seleccionado
+    setFormData({ ...formData, TECNM: value }); // Agregar el valor al estado formData
+  };
 
+  var pattern = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[-+_!@#$%^&*.,?]).+$");
 
+  const validate = () => {
+    if (formData.name === undefined) {
+      setErrors({
+        ...errors,
+        name: 'Name is required'
+      });
+      return false;
+    } else if (formData.name.length < 3) {
+      setErrors({
+        ...errors,
+        name: 'Name is too short'
+      });
+      return false;
+    } else if (!formData.pass) {
+      console.log('passs 1 if ', formData.pass)
+      setErrors({
+        ...errors,
+        pass: 'Password is required'
+      });
+      return false;
+    } else if (formData.pass.length < 8) {
+      setErrors({
+        ...errors,
+        pass: 'Pass is too short'
+      });
+      return false;
+    } else if (!pattern.test(formData.pass)) {
+      //formData.pass.search('[A-Z]')
+      console.log('pass', formData.pass)
+      setErrors({
+        ...errors,
+        pass: 'Must contain a special character'
+      });
+      return false;
+    }
 
+    setErrors({});
+    return true;
+  };
 
-    //selects
-    const handleCountryValueChange = (value) => {
-      setSelectedCountry(value); // Actualizar el valor seleccionado
-      setFormData({ ...formData, country: value }); // Agregar el valor al estado formData
-    };
-  
-    const handleTECNMValueChange = (value) => {
-      setSelectedTECNM(value); // Actualizar el valor seleccionado
-      setFormData({ ...formData, TECNM: value }); // Agregar el valor al estado formData
-    };
-  
+  const onSubmit = async () => {
+    console.log('Form Data', formData);
+    navigation.navigate(screen.authenticated);
+    console.log('You completed the form!');
+  };
 
+  return (
+    <Stack space={3} px="4" safeArea >
 
-    var pattern = new RegExp(
-      "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[-+_!@#$%^&*.,?]).+$"
-  
-    )
-  
-    const validate = () => {
-      if (formData.name === undefined) {
-        setErrors({
-          ...errors,
-          name: 'Name is required'
-        });
-        return false;
-      } else if (formData.name.length < 3) {
-        setErrors({
-          ...errors,
-          name: 'Name is too short'
-        });
-        return false;
-      } else if (!formData.pass) {
-        console.log('passs 1 if ', formData.pass)
-        setErrors({
-          ...errors,
-          pass: 'Password is required'
-        });
-        return false;
-      } else if (formData.pass.length < 8) {
-        setErrors({
-          ...errors,
-          pass: 'Pass is too short'
-        });
-        return false;
-      }
-      else if (!pattern.test(formData.pass)) {
-        //formData.pass.search('[A-Z]')
-        console.log('pass', formData.pass)
-        setErrors({
-          ...errors,
-          pass: 'Must contain a special character'
-        });
-        return false
-      }
-  
-      setErrors({})
-      return true;
-    };
-  
-    const onSubmit = async () => {
+      <Box  marginBottom={5} marginTop={5} alignSelf={"center"}>
+        <Text bold fontSize={'3xl'} >Sign up</Text>
+      </Box>
 
-      console.log('Form Data', formData);
+      <Box marginBottom={5}  alignSelf={"left"}>
+        <Text bold fontSize={'md'} >Please fill the fields</Text>
+      </Box>
 
+      <Box>
+        <HStack space={2}>
+          {/* codigo y telefono */}
+          <FormControl flex={1} isRequired isInvalid={'code' in errors}>
+            <FormControl.Label>Code</FormControl.Label>
+            <SelectCountry onValueChange={handleCountryValueChange} />
+          </FormControl>
 
-        navigation.navigate(screen.authenticated)
+          <FormControl flex={3} isRequired isInvalid={'phoneNumber' in errors}>
+            <FormControl.Label>Phone number</FormControl.Label>
+            <Input
+              placeholder="Phone number"
+              onChangeText={value => setFormData({ ...formData, phoneNumber: value })}
+            />
+          </FormControl>
+        </HStack>
+      </Box>
 
+      <Box>
+        <HStack space={2}>
+          {/* campus y numero de control */}
+          <FormControl flex={1} isRequired isInvalid={'campus' in errors}>
+            <FormControl.Label>Campus</FormControl.Label>
+            <SelectTECNM onValueChange={handleTECNMValueChange} />
+          </FormControl>
 
-        console.log('You complete the form!')
+          <FormControl flex={3} isRequired isInvalid={'controlNumber' in errors}>
+            <FormControl.Label>Control number</FormControl.Label>
+            <Input
+              placeholder="Control number"
+              onChangeText={value => setFormData({ ...formData, controlNumber: value })}
+            />
+          </FormControl>
+        </HStack>
+      </Box>
 
-  
-    };
-  
-  
-    return (
-      <ScrollView>
-        
-        <Stack space={3} alignSelf="center" px="4" safeArea mt="5"
-          w={{ base: "100%", md: "50%" }}>
-  
-  
-          <Box marginBottom={10} flex={1} alignSelf={"center"}>
-            <Text bold fontSize={'3xl'} >Sign up</Text>
-          </Box>
+      <Box>
+        {/* contraseña */}
+        <FormControl isRequired isInvalid={'pass' in errors}>
+          <FormControl.Label>Password</FormControl.Label>
+          <Input
+            type={show ? "text" : "password"}
+            InputRightElement={
+              <Pressable onPress={() => setShow(!show)}>
+                <Icon as={<Icon name={show ? "visibility" : "visibility-off"} />} size={5} mr="2" color="muted.400" />
+              </Pressable>
+            }
+            p={2}
+            placeholder="Password"
+            onChangeText={value => setFormData({ ...formData, pass: value })}
+          />
+          {'pass' in errors ?
+            <FormControl.ErrorMessage>{errors.pass}</FormControl.ErrorMessage> :
+            <FormControl.HelperText>
+              We'll keep this between us.
+            </FormControl.HelperText>
+          }
+        </FormControl>
+      </Box>
 
+      <Box>
+        <Button colorScheme="primary" variant="link" size="xs" onPress={() => navigation.navigate(screen.login)}>
+          Already have an account? Press here
+        </Button>
+      </Box>
 
-          <Box marginBottom={10} flex={1} alignSelf={"left"}>
-            <Text bold fontSize={'md'} >Please fill the fields</Text>
-          </Box>
-  
-  
+      <Box alignItems={'center'}>
+        <Button style={styles.buttonCian} onPress={onSubmit}>
+          Submit
+        </Button>
+      </Box>
+    </Stack>
+  );
+};
 
-
-          <Box>
-
-            <HStack space={2} >{/*codigo y telefono*/}
-
-
-
-
-            <FormControl flex={1} isRequired isInvalid={'code' in errors}>
-              <FormControl.Label>Code</FormControl.Label>
-              <SelectCountry onValueChange={handleCountryValueChange} />
-  
-  
-  
-            </FormControl>
-
-            <FormControl flex={3} isRequired isInvalid={'phoneNumber' in errors}>
-              <FormControl.Label>Phone number</FormControl.Label>
-              <Input  placeholder="Phone number"
-                onChangeText={value => setFormData({
-                  ...formData,
-                  phoneNumber: value
-                })} />
-  
-            </FormControl>
-
-
-
-
-
-
-            </HStack>
-
-
-
-  
-  
-          </Box>
-
-
-          <Box>
-
-            <HStack space={2} >{/*campus y numero de control*/}
-
-
-
-
-            <FormControl flex={1} isRequired isInvalid={'campus' in errors}>
-              <FormControl.Label>Campus</FormControl.Label>
-              <SelectTECNM onValueChange={handleTECNMValueChange} />
-  
-  
-  
-            </FormControl>
-
-            <FormControl flex={3} isRequired isInvalid={'controlNumber' in errors}>
-              <FormControl.Label>Control number</FormControl.Label>
-              <Input  placeholder="Control number"
-                onChangeText={value => setFormData({
-                  ...formData,
-                  controlNumber: value
-                })} />
-  
-            </FormControl>
-
-
-
-
-
-
-            </HStack>
-
-
-
-  
-  
-          </Box>
-          
-
-
-          <Box>{/*contraseña*/}
-            <FormControl isRequired isInvalid = {'pass' in errors}>
-              <FormControl.Label>Password</FormControl.Label>
-              <Input type={show ? "text" : "password" } 
-              InputRightElement={<Pressable onPress={() => setShow(!show)}>
-              <Icon as={<Icon name={show ? "visibility" : "visibility-off"} />} size={5} mr="2" color="muted.400" />
-            </Pressable>}
-              p={2} placeholder="Password"
-              onChangeText={value => setFormData({
-                ...formData,
-                pass: value
-              })} />
-              {'pass' in errors ?
-                <FormControl.ErrorMessage>{errors.pass}</FormControl.ErrorMessage>:
-              <FormControl.HelperText>
-                We'll keep this between us.
-              </FormControl.HelperText>}
-  
-            </FormControl>
-  
-  
-          </Box>
-
-          <Box>
-            <Button
-              colorScheme="primary" variant="link" size="xs"
-              onPress={() => navigation.navigate(screen.login)}>
-              Already have account? Press here
-            </Button>
-
-          
-  
-  
-          </Box>
-
-
-
-          <Box alignItems={'center'}>
-            <Button style={styles.buttonCian}
-              onPress={onSubmit}>
-              Submit
-            </Button>
-          
-
-          
-  
-  
-          </Box>
-        </Stack>
-      </ScrollView>
-    )
-}
-
-export default SignUpScreen
+export default SignUpScreen;
